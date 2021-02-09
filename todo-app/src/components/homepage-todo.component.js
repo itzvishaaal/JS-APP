556673c463de8b97ad3be03e6bcee447ea92f8b9
itzvishaaal <vishaliilm1996@gmail.com>
2021-02-09 14:39:04 +0530

    Second commit

--- todo-app/src/components/homepage-todo.component.js
+++ todo-app/src/components/homepage-todo.component.js
@@ -1,29 +1,5 @@
-import React, { Component, useEffect, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  NavLink,
-} from "react-router-dom";
-
-import {
-  Box,
-  Text,
-  Tabs,
-  Tab,
-  Button,
-  Form,
-  FormField,
-  TextInput,
-  DataTable,
-  Table,
-  TableCell,
-  TableBody,
-  TableRow,
-  TableHeader,
-  Meter,
-  CheckBox,
-} from "grommet";
+import React from "react";
+import { Table, TableCell, TableBody, TableRow, TableHeader } from "grommet";
 
 const ToDoList = () => {
   let array = JSON.parse(localStorage.getItem("user"));
@@ -36,48 +12,48 @@ const ToDoList = () => {
     let password = array[keys].Password;
     let firstName = array[keys].FirstName;
     let lastName = array[keys].LastName;
-    let confirmPassword = array[keys].confirmPassword;
+    let confirmPassword = array[keys].ConfirmPassword;
     let phone = array[keys].Phone;
+    return (
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableCell scope="col" border="bottom">
+              <strong>ID</strong>
+            </TableCell>
+            <TableCell scope="col" border="bottom">
+              <strong>First Name</strong>
+            </TableCell>
+            <TableCell scope="col" border="bottom">
+              <strong>Last Name</strong>
+            </TableCell>
+            <TableCell scope="col" border="bottom">
+              <strong>Email</strong>
+            </TableCell>
+            <TableCell scope="col" border="bottom">
+              <strong>Password</strong>
+            </TableCell>
+            <TableCell scope="col" border="bottom">
+              <strong>Confirm Password</strong>
+            </TableCell>
+            <TableCell scope="col" border="bottom">
+              <strong>Phone</strong>
+            </TableCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell scope="row">{id}</TableCell>
+            <TableCell>{firstName}</TableCell>
+            <TableCell>{lastName}</TableCell>
+            <TableCell>{email}</TableCell>
+            <TableCell>{password}</TableCell>
+            <TableCell>{confirmPassword}</TableCell>
+            <TableCell>{phone}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    );
   }
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableCell scope="col" border="bottom">
-            First Name
-          </TableCell>
-          <TableCell scope="col" border="bottom">
-            Last Name
-          </TableCell>
-          <TableCell scope="col" border="bottom">
-            Email
-          </TableCell>
-          <TableCell scope="col" border="bottom">
-            Password
-          </TableCell>
-          <TableCell scope="col" border="bottom">
-            Confirm Password
-          </TableCell>
-          <TableCell scope="col" border="bottom">
-            Phone
-          </TableCell>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell scope="row">
-            <strong>Eric</strong>
-          </TableCell>
-          <TableCell>Coconut</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell scope="row">
-            <strong>Chris</strong>
-          </TableCell>
-          <TableCell>Watermelon</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
-  );
 };
 export default ToDoList;
