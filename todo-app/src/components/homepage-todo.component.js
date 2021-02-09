270947dad6d8d36c8b10e9e43fbd17bfdf2377ea
itzvishaaal <vishaliilm1996@gmail.com>
2021-02-09 18:11:40 +0530

    Third commit

--- todo-app/src/components/homepage-todo.component.js
+++ todo-app/src/components/homepage-todo.component.js
@@ -1,59 +1,57 @@
 import React from "react";
+import SignInForm from "./signIn-todo.component";
 import { Table, TableCell, TableBody, TableRow, TableHeader } from "grommet";
 
 const ToDoList = () => {
   let array = JSON.parse(localStorage.getItem("user"));
 
-  for (const prop in array) {
-    var keys = [];
-    keys.push(prop);
-    let id = array[keys].ID;
-    let email = array[keys].Email;
-    let password = array[keys].Password;
-    let firstName = array[keys].FirstName;
-    let lastName = array[keys].LastName;
-    let confirmPassword = array[keys].ConfirmPassword;
-    let phone = array[keys].Phone;
-    return (
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableCell scope="col" border="bottom">
-              <strong>ID</strong>
-            </TableCell>
-            <TableCell scope="col" border="bottom">
-              <strong>First Name</strong>
-            </TableCell>
-            <TableCell scope="col" border="bottom">
-              <strong>Last Name</strong>
-            </TableCell>
-            <TableCell scope="col" border="bottom">
-              <strong>Email</strong>
-            </TableCell>
-            <TableCell scope="col" border="bottom">
-              <strong>Password</strong>
-            </TableCell>
-            <TableCell scope="col" border="bottom">
-              <strong>Confirm Password</strong>
-            </TableCell>
-            <TableCell scope="col" border="bottom">
-              <strong>Phone</strong>
-            </TableCell>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell scope="row">{id}</TableCell>
-            <TableCell>{firstName}</TableCell>
-            <TableCell>{lastName}</TableCell>
-            <TableCell>{email}</TableCell>
-            <TableCell>{password}</TableCell>
-            <TableCell>{confirmPassword}</TableCell>
-            <TableCell>{phone}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    );
-  }
+  const key = [];
+  let id = array[key].ID;
+  let email1 = array[key].Email;
+  let password = array[key].Password;
+  let firstName = array[key].FirstName;
+  let lastName = array[key].LastName;
+  let confirmPassword = array[key].ConfirmPassword;
+  let phone = array[key].Phone;
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableCell scope="col" border="bottom">
+            <strong>ID</strong>
+          </TableCell>
+          <TableCell scope="col" border="bottom">
+            <strong>First Name</strong>
+          </TableCell>
+          <TableCell scope="col" border="bottom">
+            <strong>Last Name</strong>
+          </TableCell>
+          <TableCell scope="col" border="bottom">
+            <strong>Email</strong>
+          </TableCell>
+          <TableCell scope="col" border="bottom">
+            <strong>Password</strong>
+          </TableCell>
+          <TableCell scope="col" border="bottom">
+            <strong>Confirm Password</strong>
+          </TableCell>
+          <TableCell scope="col" border="bottom">
+            <strong>Phone</strong>
+          </TableCell>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell scope="row">{id}</TableCell>
+          <TableCell>{firstName}</TableCell>
+          <TableCell>{lastName}</TableCell>
+          <TableCell>{email1}</TableCell>
+          <TableCell>{password}</TableCell>
+          <TableCell>{confirmPassword}</TableCell>
+          <TableCell>{phone}</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
 };
 export default ToDoList;
